6f26123f2066e26f135c2e77566ae630ee358ca8
SunnyRichman <109719498+SunnyRichman@users.noreply.github.com>
2025-02-27 17:55:01 +0700

    Update TradeIn.java

--- HolidayTravelVehicles/TradeIn.java
+++ HolidayTravelVehicles/TradeIn.java
@@ -8,4 +8,33 @@ public class TradeIn{
     this.make = _make;
     this.year = _year;
   }
+
+  public void setSerialNum(int _serialNum) {
+      this.serialNum = _serialNum;
+  }
+  
+  public void setMake(String _make) {
+      this.make = _make;
+  }
+  
+  public void setYear(int _year) {
+      this.year = _year;
+  }
+  
+  public int getSerialNum() {
+      return serialNum;
+  }
+  
+  public String getMake() {
+      return make;
+  }
+  
+  public int getYear() {
+      return year;
+  }
+  
+  public String log() {
+      return getSerialNum() + "\t" + getMake() + "\t" + getYear();
+  }
+
 }
