f71f5e0e5f24a3a6c9cc28e2c6820151a25e6383
SunnyRichman <109719498+SunnyRichman@users.noreply.github.com>
2025-02-27 17:50:43 +0700

    Update Dealer.java

--- HolidayTravelVehicles/Dealer.java
+++ HolidayTravelVehicles/Dealer.java
@@ -8,4 +8,33 @@ public class Dealer{
     this.description = desc;
     this.price = _price;
   }
+
+  public void setOptionCode(int _optionCode) {
+      this.optionCode = _optionCode;
+  }
+  
+  public void setDescription(String _description) {
+      this.description = _description;
+  }
+  
+  public void setPrice(int _price) {
+      this.price = _price;
+  }
+
+  public int getOptionCode() {
+      return optionCode;
+  }
+  
+  public String getDescription() {
+      return description;
+  }
+  
+  public int getPrice() {
+      return price;
+  }
+  
+  public String log() {
+      return getOptionCode() + "\t" + getDescription() + "\t" + getPrice();
+  }
+
 }
