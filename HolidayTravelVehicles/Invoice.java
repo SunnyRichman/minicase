bd40a4a8a8e8da3f370bef65d511946652543a47
SunnyRichman <109719498+SunnyRichman@users.noreply.github.com>
2025-02-27 17:52:56 +0700

    Update Invoice.java

--- HolidayTravelVehicles/Invoice.java
+++ HolidayTravelVehicles/Invoice.java
@@ -14,4 +14,57 @@ public class Invoice{
     this.negotiatedPrice = Price;
     this.taxesLicenseFee = _taxesLicenseFee;
   }
+
+  public void setFullCustomerInfo(Customer _fullCustomerInfo) {
+      this.fullCustomerInfo = _fullCustomerInfo;
+  }
+  
+  public void setTradeInVecInfo(String _tradeInVecInfo) {
+      this.tradeInVecInfo = _tradeInVecInfo;
+  }
+  
+  public void setPurchaseVecInfo(String _purchaseVecInfo) {
+      this.purchaseVecInfo = _purchaseVecInfo;
+  }
+  
+  public void setDealerOptions(String _dealerOptions) {
+      this.dealerOptions = _dealerOptions;
+  }
+  
+  public void setNegotiatedPrice(int _negotiatedPrice) {
+      this.negotiatedPrice = _negotiatedPrice;
+  }
+  
+  public void setTaxesLicenseFee(int _taxesLicenseFee) {
+      this.taxesLicenseFee = _taxesLicenseFee;
+  }
+  
+  public Customer getFullCustomerInfo() {
+      return fullCustomerInfo;
+  }
+  
+  public String getTradeInVecInfo() {
+      return tradeInVecInfo;
+  }
+  
+  public String getPurchaseVecInfo() {
+      return purchaseVecInfo;
+  }
+  
+  public String getDealerOptions() {
+      return dealerOptions;
+  }
+  
+  public int getNegotiatedPrice() {
+      return negotiatedPrice;
+  }
+  
+  public int getTaxesLicenseFee() {
+      return taxesLicenseFee;
+  }
+  
+  public String log() {
+      return getFullCustomerInfo() + "\t" + getTradeInVecInfo() + "\t" + getPurchaseVecInfo() + "\t" + getDealerOptions() + "\t" + getNegotiatedPrice() + "\t" + getTaxesLicenseFee();
+  }
+
 }
