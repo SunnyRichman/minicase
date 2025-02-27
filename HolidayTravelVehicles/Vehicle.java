c8073e7224df605c5babff1f024ce19739735ffb
SunnyRichman <109719498+SunnyRichman@users.noreply.github.com>
2025-02-27 17:56:28 +0700

    Update Vehicle.java

--- HolidayTravelVehicles/Vehicle.java
+++ HolidayTravelVehicles/Vehicle.java
@@ -16,7 +16,56 @@ public class Vehicle{
     this.baseCost = _base;
   }
 
-  public String getVehicleInfo(){
-    return "Serial Number: "+this.serialNum+"\tName: "+this.name+"\tModel: "+this.model+"\tYear: "+this.year+"\tManufacturer: "+this.manufacturer+"\tBase cost: "+this.baseCost;
+  public void setSerialNum(int _serialNum) {
+      this.serialNum = _serialNum;
   }
+  
+  public void setName(String _name) {
+      this.name = _name;
+  }
+  
+  public void setModel(String _model) {
+      this.model = _model;
+  }
+  
+  public void setYear(int _year) {
+      this.year = _year;
+  }
+  
+  public void setManufacturer(String _manufacturer) {
+      this.manufacturer = _manufacturer;
+  }
+  
+  public void setBaseCost(int _baseCost) {
+      this.baseCost = _baseCost;
+  }
+  
+  public int getSerialNum() {
+      return serialNum;
+  }
+  
+  public String getName() {
+      return name;
+  }
+  
+  public String getModel() {
+      return model;
+  }
+  
+  public int getYear() {
+      return year;
+  }
+  
+  public String getManufacturer() {
+      return manufacturer;
+  }
+  
+  public int getBaseCost() {
+      return baseCost;
+  }
+  
+  public String log() {
+      return getSerialNum() + "\t" + getName() + "\t" + getModel() + "\t" + getYear() + "\t" + getManufacturer() + "\t" + getBaseCost();
+  }
+
 }
