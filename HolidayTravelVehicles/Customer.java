7cca550469a44f275ac5b98a94b60be6a65b43f6
SunnyRichman <109719498+SunnyRichman@users.noreply.github.com>
2025-02-27 17:49:29 +0700

    Update Customer.java

--- HolidayTravelVehicles/Customer.java
+++ HolidayTravelVehicles/Customer.java
@@ -11,5 +11,41 @@ public class Customer{
     this.address = addr;
     this.phoneNum = _phoneNum;
   }
+
+  public void setID(int _ID) {
+    this.ID = _ID;
+  }
+
+  public void setName(String _name) {
+      this.name = _name;
+  }
   
+  public void setAddress(String _address) {
+      this.address = _address;
+  }
+  
+  public void setPhoneNum(String _phoneNum) {
+      this.phoneNum = _phoneNum;
+  }
+
+  public int getID() {
+    return ID;
+}
+
+  public String getName() {
+      return name;
+  }
+  
+  public String getAddress() {
+      return address;
+  }
+  
+  public String getPhoneNum() {
+      return phoneNum;
+  }
+
+  public String log() {
+    return getID() + "\t" + getName() + "\t" + getAddress() + "\t" + getPhoneNum();
+  }
+
 }
