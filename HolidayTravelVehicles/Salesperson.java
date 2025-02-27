dad425ce2abcd119164bf9985a9c67166a489c1c
SunnyRichman <109719498+SunnyRichman@users.noreply.github.com>
2025-02-27 17:53:57 +0700

    Update Salesperson.java

--- HolidayTravelVehicles/Salesperson.java
+++ HolidayTravelVehicles/Salesperson.java
@@ -10,4 +10,32 @@ public class Salesperson{
     this.address = addr;
   }
   
+  public void setID(int _ID) {
+      this.ID = _ID;
+  }
+  
+  public void setPhoneNum(String _phoneNum) {
+      this.phoneNum = _phoneNum;
+  }
+  
+  public void setAddress(String _address) {
+      this.address = _address;
+  }
+  
+  public int getID() {
+      return ID;
+  }
+  
+  public String getPhoneNum() {
+      return phoneNum;
+  }
+  
+  public String getAddress() {
+      return address;
+  }
+  
+  public String log() {
+      return getID() + "\t" + getPhoneNum() + "\t" + getAddress();
+  }
+  
 }
